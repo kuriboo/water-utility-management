dd1306ca9dcadb1e6d4a7ba05e0b3c2a3f2dc45b
kuriboo <kuriboo5@gmail.com>
2024-09-13 19:55:51 +0900

    any型の使用をやめるよう変更

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextRequest, NextResponse } from 'next/server';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -24,9 +25,9 @@ export const createClientSideClient = () => {
 }
 
 // ミドルウェアの使用のために更新
-export const createMiddlewareSupabaseClient = (context: { req: any; res: any }) => {
-    return createMiddlewareClient(
-      { req: context.req, res: context.res },
-      { supabaseUrl: supabaseUrl, supabaseKey: supabaseAnonKey }
-    )
+export const createMiddlewareSupabaseClient = (context: { req: NextRequest; res: NextResponse }) => {
+  return createMiddlewareClient(
+    context,
+    { supabaseUrl: supabaseUrl, supabaseKey: supabaseAnonKey }
+  )
 }
